35ffdc12fd867d5d9390840de1d9b0678b95f047
Jeff Pelletier <jpell185@yahoo.com>
2019-04-10 10:06:19 -0400

    add custom fonts and update home text

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -12,7 +12,14 @@ export class AppHome {
 
                 <p>I'm a designer who's been coding professionally for over 15 years. <br /> I love technology, and write about it (when lightning strikes).</p>
 
-                <p>You're viewing the Stencil version of my portfolio. There's also <a href="http://jeffpelletier.me">a static HTML version</a>, if that's your flavor.</p>
+                <p>I've been experimenting and built 3 different versions of my portfolio. You're currently viewing the <strong>Stencil</strong> version.</p>
+
+                <p>These additional version are available (note which version you're viewing via the URL bar):</p>
+
+                <ul>
+                    <li><a href="http://jeffpelletier.me">Vanilla HTML/CSS/JS Portfolio</a></li>
+                    <li><a href="http://gatsby.jeffpelletier.me">Gatsby Portfolio</a>. More about <a data-hook="link-external" href="https://www.gatsbyjs.org/" rel="external">Gatsby</a>.</li>
+                </ul>
 
                 <p>Check out some of my recent <stencil-route-link url="/work">work</stencil-route-link>, some of the <stencil-route-link url="/writing">words I've written</stencil-route-link>, and <a href="mailto:jeffrey.k.pelletier+folio@gmail.com">get in touch</a> if you want to chat.</p>
             </div>
